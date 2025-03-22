8c5f98e2899770a68d888feb692fbc9f80f2ea8e
ankyranalytics <ankyranalytics@gmail.com>
2025-03-22 14:56:35 +0300

    Delete product-app directory

--- product-app/domain/product.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-type Product struct {
-	Id       int64
-	Name     string
-	Price    float32
-	Discount float32
-	Store    string
-}
--- product-app/service/model/model.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type ProductCreate struct {
-	Name     string
-	Price    float32
-	Discount float32
-	Store    string
-}
